a617396e795ed3154acba473c2b2fe4f7df4c845
rpturbina <rizki.pratama.turbina@gmail.com>
2022-10-28 07:36:18 +0700

    chore: update meta tags

--- components/DefaultLayout.tsx
+++ components/DefaultLayout.tsx
@@ -11,6 +11,37 @@ const DefaultLayout = ({ children }: { children: React.ReactNode }) => {
           name="description"
           content="Hello World👋! I'm Rizki Pratama Turbina. Frontend Web Enthusiast | Bring my frontend experiences to solve your problems."
         />
+
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://rpturbina.me/" />
+        <meta
+          property="og:title"
+          content="rpturbina | Personal Website by Rizki Pratama Turbina"
+        />
+        <meta
+          property="og:description"
+          content="Hello World👋! I'm Rizki Pratama Turbina. Frontend Web Enthusiast | Bring my frontend experiences to solve your problems."
+        />
+        <meta property="og:image" content="/images/web-thumbnail.png" />
+
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta property="twitter:url" content="https://rpturbina.me/" />
+        <meta
+          property="twitter:title"
+          content="rpturbina | Personal Website by Rizki Pratama Turbina"
+        />
+        <meta
+          property="twitter:description"
+          content="Hello World👋! I'm Rizki Pratama Turbina. Frontend Web Enthusiast | Bring my frontend experiences to solve your problems."
+        />
+        <meta
+          property="twitter:image"
+          content="https://rpturbina.me/src/images/meta-tags-rpturbina.me.png"
+        />
+        <meta
+          name="keywords"
+          content="Personal Website by Rizki Pratama Turbina, rpturbina personal website"
+        />
       </Head>
       <Header />
       <main className="mx-auto max-w-5xl">{children}</main>
